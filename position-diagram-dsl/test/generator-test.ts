7c7f9cc611a67d3ca28c5962a05ade63505e0485
Johannes Meier <johannes.meier@uni-oldenburg.de>
2023-01-02 11:46:54 +0100

    improved the test case generator-test.ts with checks of the EMF model

--- position-diagram-dsl/test/generator-test.ts
+++ position-diagram-dsl/test/generator-test.ts
@@ -6,7 +6,7 @@ describe('test the TikZ generator', () => {
         // build the EMF model
         let model : Model = {
             $type : 'Model',
-            nodes : [], // Node ergänzen?? ist das automatisch bidirektional?
+            nodes : [],
             edges : []
         };
         let node : Node = {
@@ -14,16 +14,25 @@ describe('test the TikZ generator', () => {
             $container : model,
             name : "n1"
         };
-        let generator : PositionNormal = {
+        let position : PositionNormal = {
             $type : 'PositionNormal',
             $container : node,
             posX : "2mm",
             posY : "-3mm",
-            xshift : "", // optional argument ?!
+            xshift : "", // TODO: why is this required? it should be an optional argument according to the grammar!
             yshift : ""
         };
+        node.position = position;
+        model.nodes[0] = node;
+
+        // test the EMF model
+        expect(position.$container).toBe(node);
+        expect(node.position).toBe(position);
+        expect(model.nodes.length).toBe(1);
+        expect(model.nodes.indexOf(node)).toBe(0);
+        expect(model.edges.length).toBe(0);
 
         // test the generated TikZ string
-        expect(printPositionNormal(generator)).toBe("2mm, -3mm");
+        expect(printPositionNormal(position)).toBe("2mm, -3mm");
     });
 });
